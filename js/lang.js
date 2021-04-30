bcf4bb24c44ca9c28369953de85a484fedeef126
Dominik <d>
2021-04-30 10:11:28 +0200

    Added lang-switch.

---
+++ js/lang.js
@@ -0,0 +1,16 @@
+lang = {
+    "de": {
+        "lang-name": "Deutsch",
+        "main-title": "Tourismus App <span class=\"orange\">Le<span id=\"heart\">❤</span>nding</span>"
+    },
+    "en": {
+        "lang-name": "English",
+        "main-title": "Tourism App <span class=\"orange\">Le<span id=\"heart\">❤</span>nding</span>"
+    }
+}
+
+// Only loads de package right now, will get changed in future
+function loadLanguage(lange) {
+    document.getElementById("lang-header").innerText = lang[lange]["lang-name"];
+    document.getElementById("main-title").innerHTML = lang[lange]["main-title"];
+}
