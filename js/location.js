628b2954552227f589220a5e8cad7c4fc011b8f1
it180184 <f.ettinger@students.htl-leonding.ac.at>
2021-05-25 10:11:08 +0200

    update location feature

--- js/location.js
+++ js/location.js
@@ -21,27 +21,40 @@ function loadGeoLocation(success, error, loaded) {
     }
 }
 
-function getUserLocation() {
+async function getUserLocation(user) {
+    if (!user)
+        user = await getUser();
+    if (user.error && user.error.length > 0) {
+        return user.error;
+    }
+    return `Lat: ${user.lat.toFixed(5)}, Long: ${user.long.toFixed(5)}`;
+}
+
+function getUser() {
     return new Promise((resolve) => {
         loadGeoLocation(
             (pos) => {
                 let user = new User(pos.coords.latitude, pos.coords.longitude);
                 sessionStorage.setItem('user', JSON.stringify(user));
-                resolve(`Lat: ${user.lat}, Long: ${user.long}`);
+                resolve(user);
             },
             () => {
                 let user = User.Error('No Location Available');
                 sessionStorage.setItem('user', JSON.stringify(user));
-                resolve(user.error);
+                resolve(user);
             },
             () => {
                 let user = JSON.parse(sessionStorage.getItem('user'));
-                resolve(
-                    user.error
-                        ? user.error
-                        : `Lat: ${user.lat}, Long: ${user.long}`
-                );
+                resolve(user);
             }
         );
     });
+}
+
+if (window.location.href.includes("offies.html")) {
+    window.onload = () => {
+        getUserLocation().then(loc => {
+            document.getElementById('location-message').innerText = loc;
+        })
+    };
 }
