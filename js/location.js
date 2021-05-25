26fc28ec7a4c48447d21e9756b370e8b098c02af
IT180175 <57949805+IT180175@users.noreply.github.com>
2021-05-25 11:42:01 +0200

    Update location.js
    Berechnung von Distanz

--- js/location.js
+++ js/location.js
@@ -58,3 +58,39 @@ if (window.location.href.includes("offies.html")) {
         })
     };
 }
+
+function getDistance() {
+
+    let lat1 = document.querySelector("#standortx1").value;
+    console.log(lat1);
+
+    let long1 = document.querySelector("#standorty1").value;
+    console.log(long1);
+
+    let lat2 = document.getElementById("standortx2").value;
+    console.log(lat2);
+
+    let long2 = document.querySelector("#standorty2").value;
+    console.log(long2);
+
+    const equatorLength = 40000;
+    const lat1Length = Math.cos(lat1) * equatorLength;
+    const lat2Length = Math.cos(lat2) * equatorLength;
+
+    const latDiff = Math.abs(lat1 - lat2);
+    const longDiff = Math.abs(long1 - long2);
+
+    let dist1 = Math.sqrt(
+        Math.pow((lat1Length / 360) * longDiff, 2) +
+            Math.pow((equatorLength / 360) * latDiff, 2)
+    );
+    let dist2 = Math.sqrt(
+        Math.pow((lat2Length / 360) * longDiff, 2) +
+            Math.pow((equatorLength / 360) * latDiff, 2)
+    );
+    let dist = (dist1 + dist2) / 2;
+
+    console.log(dist);
+
+    return dist;
+}
