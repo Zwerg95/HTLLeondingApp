7a9573cc4f99a8037ca6d2201a2dfe086bf77aec
David Ursprung <59611676+DavidUrsprung@users.noreply.github.com>
2021-06-11 08:47:29 +0200

    Update insert_tram.sql

--- postgres/insert_tram.sql
+++ postgres/insert_tram.sql
@@ -1,12 +1,12 @@
 /*Linie 3*/
-insert into Adresse(plz, ort, strasse) values(4040, 'Linz', 'Landgutstraße');
+insert into Adresse(plz, ort, strasse) values(4040, 'Linz', 'LandgutstraÃŸe');
 insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'Hauptplatz');
-insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'Landstraße');
-insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'Kärntnerstraße');
-insert into Adresse(plz, ort, strasse) values(4060, 'Leonding', 'Gaumbergstraße');
-insert into Adresse(plz, ort, strasse) values(4060, 'Leonding', 'Ehrenfellner Straße');
-insert into Adresse(plz, ort, strasse) values(4061, 'Pasching', 'Kremstalstraße');
-insert into Adresse(plz, ort, strasse) values(4050, 'Traun', 'Kürnbergstraße');
+insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'LandstraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'KÃ¤rntnerstraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4060, 'Leonding', 'GaumbergstraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4060, 'Leonding', 'Ehrenfellner StraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4061, 'Pasching', 'KremstalstraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4050, 'Traun', 'KÃ¼rnbergstraÃŸe');
 
 insert into Standort(xKoordinate, yKoordinate, adresse) values(48.311079, 14.277665, 13);
 insert into Standort(xKoordinate, yKoordinate, adresse) values(48.306544, 14.285989, 14);
@@ -17,7 +17,7 @@ insert into Standort(xKoordinate, yKoordinate, adresse) values(48.26855273180912
 insert into Standort(xKoordinate, yKoordinate, adresse) values(48.243070, 14.237171, 19);
 insert into Standort(xKoordinate, yKoordinate, adresse) values(48.233795, 14.234922, 20);
 
-insert into Haltestelle(name, standort) values('Landgutstraße', 13);
+insert into Haltestelle(name, standort) values('LandgutstraÃŸe', 13);
 insert into Haltestelle(name, standort) values('Hauptplatz', 14);
 insert into Haltestelle(name, standort) values('Taubenmarkt', 15);
 insert into Haltestelle(name, standort) values('Hauptbahnhof', 16);
@@ -28,32 +28,15 @@ insert into Haltestelle(name, standort) values('Trauner Kreuzung', 20);
 
 insert into linie(name) values('3');
 
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(1, 2, 28, null);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(2, 3, 20, 1);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(3, 4, 15, 2);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(4, 5, 4, 3);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(5, 6, 2, 4);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(6, 7, 23, 5);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor, danach) values(7, 8, 33, 6, null);
-
-update teilstrecke set danach = 2 where id = 1;
-update teilstrecke set danach = 3 where id = 2;
-update teilstrecke set danach = 4 where id = 3;
-update teilstrecke set danach = 5 where id = 4;
-update teilstrecke set danach = 6 where id = 5;
-update teilstrecke set danach = 7 where id = 6;
-
-update teilstrecke set oe_linie = '3' where 1 = 1;
-
 /*Linie 4*/
-insert into Adresse(plz, ort, strasse) values(4040, 'Linz', 'Landgutstraße');
+insert into Adresse(plz, ort, strasse) values(4040, 'Linz', 'LandgutstraÃŸe');
 insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'Hauptplatz');
-insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'Landstraße');
-insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'Kärntnerstraße');
-insert into Adresse(plz, ort, strasse) values(4060, 'Leonding', 'Ehrenfellner Straße');
-insert into Adresse(plz, ort, strasse) values(4061, 'Pasching', 'Kremstalstraße');
-insert into Adresse(plz, ort, strasse) values(4050, 'Traun', 'Kürnbergstraße');
-insert into Adresse(plz, ort, strasse) values(4050, 'Traun', 'Kremstalstraße');
+insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'LandstraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4020, 'Linz', 'KÃ¤rntnerstraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4060, 'Leonding', 'Ehrenfellner StraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4061, 'Pasching', 'KremstalstraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4050, 'Traun', 'KÃ¼rnbergstraÃŸe');
+insert into Adresse(plz, ort, strasse) values(4050, 'Traun', 'KremstalstraÃŸe');
 
 insert into Standort(xKoordinate, yKoordinate, adresse) values(48.311079, 14.277665, 21);
 insert into Standort(xKoordinate, yKoordinate, adresse) values(48.306544, 14.285989, 22);
@@ -64,7 +47,7 @@ insert into Standort(xKoordinate, yKoordinate, adresse) values(48.243070, 14.237
 insert into Standort(xKoordinate, yKoordinate, adresse) values(48.233795, 14.234922, 27);
 insert into Standort(xKoordinate, yKoordinate, adresse) values(48.218587, 14.238541, 28);
 
-insert into Haltestelle(name, standort) values('Landgutstraße', 21);
+insert into Haltestelle(name, standort) values('LandgutstraÃŸe', 21);
 insert into Haltestelle(name, standort) values('Hauptplatz', 22);
 insert into Haltestelle(name, standort) values('Taubenmarkt', 23);
 insert into Haltestelle(name, standort) values('Hauptbahnhof', 24);
@@ -74,20 +57,3 @@ insert into Haltestelle(name, standort) values('Trauner Kreuzung', 27);
 insert into Haltestelle(name, standort) values('Schloss Traun', 28);
 
 insert into linie(name) values('4');
-
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(1, 2, 28, null);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(2, 3, 20, 1);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(3, 4, 15, 2);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(4, 5, 4, 3);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(5, 6, 2, 4);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor) values(6, 7, 23, 5);
-insert into TeilStrecke(startHaltestelle, zielHaltestelle, dauer, davor, danach) values(7, 8, 33, 6, null);
-
-update teilstrecke set danach = 2 where id = 1;
-update teilstrecke set danach = 3 where id = 2;
-update teilstrecke set danach = 4 where id = 3;
-update teilstrecke set danach = 5 where id = 4;
-update teilstrecke set danach = 6 where id = 5;
-update teilstrecke set danach = 7 where id = 6;
-
-update teilstrecke set oe_linie = '4' where 1 = 1;
